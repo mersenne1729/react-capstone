e0abab8c661a10183d8f9fc142694596da28604a
Thomas <kent.thomas80@gmail.com>
2017-02-21 17:05:32 +0000

    React added

--- data/types.js
+++ data/types.js
@@ -1,52 +1,64 @@
-var typeArray = [ 
+var typeArray = [
 
-//Type 0
+    //Type 0
 
-{ "Type":0,
-"summary": "Our currnt stiuation -We’re currently at about 0.73 on the Kardashev Scale. It is presumed we’ll reach type I in about 100 years, give or take, depending on how fast our technology advances and how diligently we procreate.",
-"energy-consumption": "1.5^20 erg/sec",
-"sci-fi-example": "Humans",
-"image":"homer.jpg"},
+    {
+        "type": 0,
+        "summary": "Our currnt stiuation -We’re currently at about 0.73 on the Kardashev Scale. It is presumed we’ll reach type I in about 100 years, give or take, depending on how fast our technology advances and how diligently we procreate.",
+        "energy-consumption": "1.5^20 erg/sec",
+        "sci-fi-example": "Humans",
+        "image": "homer.jpg"
+    },
 
-//Type 1
+    //Type 1
 
-{ "Type":1,
-"summary":"A Type I designation is a given to species who have been able to harness all the energy that is available from a neighboring star, gathering and storing it to meet the energy demands of a growing population. This means that we would need to boost our current energy production over 100,000 times to reach this status. However, being able to harness all Earth’s energy would also mean that we could have control over all natural forces. Human beings could control volcanoes, the weather, and even earthquakes! (At least, that is the idea.) These kinds of feats are hard to believe, but compared to the advances that may still be to come, these are just basic and primitive levels of control (it’s absolutely nothing compared to the capabilities of societies with higher rankings).",
-"energy-consumption":"4 x 10^19 erg/sec",
-"sci-fi-example":"New Xindus Cooperative ",
-"image":"xindi.jpg"},
+    {
+        "type": 1,
+        "summary": "A Type I designation is a given to species who have been able to harness all the energy that is available from a neighboring star, gathering and storing it to meet the energy demands of a growing population. This means that we would need to boost our current energy production over 100,000 times to reach this status. However, being able to harness all Earth’s energy would also mean that we could have control over all natural forces. Human beings could control volcanoes, the weather, and even earthquakes! (At least, that is the idea.) These kinds of feats are hard to believe, but compared to the advances that may still be to come, these are just basic and primitive levels of control (it’s absolutely nothing compared to the capabilities of societies with higher rankings).",
+        "energy-consumption": "4 x 10^19 erg/sec",
+        "sci-fi-example": "New Xindus Cooperative ",
+        "image": "xindi.jpg"
+    },
 
-//Type 2
+    //Type 2
 
-{"Type":2,
-"summary":"A Type II civilization – can harness the power of their entire star (not merely transforming starlight into energy, but controlling the star). Several methods for this have been proposed. The most popular of which is the hypothetical ‘Dyson Sphere.’ This device, if you want to call it that, would encompass every single inch of the star, gathering most (if not all) of its energy output and transferring it to a planet for later use. Alternatively, if fusion power (the mechanism that powers stars) had been mastered by the race, a reactor on a truly immense scale could be used to satisfy their needs. Nearby gas giants can be utilized for their hydrogen, slowly drained of life by an orbiting reactor. What would this much energy mean for a species? Well, nothing known to science could wipe out a Type II civilization. Take, for instance, if humans survived long enough to reach this status, and a moon sized object entered our solar system on a collision course with our little blue planet–we’d have the ability to vaporize it out of existence. Or if we had time, we could move our planet out of the way, completely dodging it. But let’s say we didn’t want to move Earth… are there any other options? Well yes, because we’d have the capability to move Jupiter, or another planet of our choice, into the way – pretty cool, right?",
-"energy-consumption":"4 x 10^33 erg/sec",
-"sci-fi-example":"Royal Kingdom of Betazed",
-"image":"betazed.jpg"},
+    {
+        "type": 2,
+        "summary": "A Type II civilization – can harness the power of their entire star (not merely transforming starlight into energy, but controlling the star). Several methods for this have been proposed. The most popular of which is the hypothetical ‘Dyson Sphere.’ This device, if you want to call it that, would encompass every single inch of the star, gathering most (if not all) of its energy output and transferring it to a planet for later use. Alternatively, if fusion power (the mechanism that powers stars) had been mastered by the race, a reactor on a truly immense scale could be used to satisfy their needs. Nearby gas giants can be utilized for their hydrogen, slowly drained of life by an orbiting reactor. What would this much energy mean for a species? Well, nothing known to science could wipe out a Type II civilization. Take, for instance, if humans survived long enough to reach this status, and a moon sized object entered our solar system on a collision course with our little blue planet–we’d have the ability to vaporize it out of existence. Or if we had time, we could move our planet out of the way, completely dodging it. But let’s say we didn’t want to move Earth… are there any other options? Well yes, because we’d have the capability to move Jupiter, or another planet of our choice, into the way – pretty cool, right?",
+        "energy-consumption": "4 x 10^33 erg/sec",
+        "sci-fi-example": "Royal Kingdom of Betazed",
+        "image": "betazed.jpg"
+    },
 
-//Type 3
+    //Type 3
 
-{"Type":3,
-"summary":"A Type III, where a species then becomes galactic traversers with knowledge of everything having to do with energy, resulting in them becoming a master race.  In terms of humans, hundreds of thousands of years of evolution – both biological and mechanical – may result in the inhabitants of this type III civilization being incredibly different from the human race as we know it. These may be cyborgs (or cybernetic organism, beings both biological and robotic), with the descendants of regular humans being a sub-species among the now-highly advanced society. These wholly biological humans would likely be seen as being disabled, inferior, or unevolved by their cybernetic counterparts.At this stage, we would have developed colonies of robots that are capable of ‘self replication’; their population may increase into the millions as they spread out across the galaxy, colonizing star after star. And these being might build Dyson Spheres to encapsulate each one, creating a huge network that would carry energy back to the home planet. But stretching over the galaxy in such a manner would face several problems; namely, the species would be constrained by the laws of physics. Particularly, light-speed travel. That is, unless they develop a working warp drive, or use that immaculate energy cache to master wormhole teleportation (two things that remain theoretical for the time being), they can only get so far.",
-"energy-consumption":"4 x 10^44 erg/sec",
-"sci-fi-example":"The Q Continum",
-"image":"theq.jpg"},
+    {
+        "type": 3,
+        "summary": "A Type III, where a species then becomes galactic traversers with knowledge of everything having to do with energy, resulting in them becoming a master race.  In terms of humans, hundreds of thousands of years of evolution – both biological and mechanical – may result in the inhabitants of this type III civilization being incredibly different from the human race as we know it. These may be cyborgs (or cybernetic organism, beings both biological and robotic), with the descendants of regular humans being a sub-species among the now-highly advanced society. These wholly biological humans would likely be seen as being disabled, inferior, or unevolved by their cybernetic counterparts.At this stage, we would have developed colonies of robots that are capable of ‘self replication’; their population may increase into the millions as they spread out across the galaxy, colonizing star after star. And these being might build Dyson Spheres to encapsulate each one, creating a huge network that would carry energy back to the home planet. But stretching over the galaxy in such a manner would face several problems; namely, the species would be constrained by the laws of physics. Particularly, light-speed travel. That is, unless they develop a working warp drive, or use that immaculate energy cache to master wormhole teleportation (two things that remain theoretical for the time being), they can only get so far.",
+        "energy-consumption": "4 x 10^44 erg/sec",
+        "sci-fi-example": "The Q Continum",
+        "image": "theq.jpg"
+    },
 
-//Type 4
+    //Type 4
 
-{"Type":4,
-"summary":"A Type IV civilizations would almost be able to harness the energy content of the entire universe and with that, they could traverse the accelerating expansion of space (furthermore, advance races of these species may live inside supermassive black holes). To previous methods of generating energy, these kinds of feats are considered impossible. A Type IV civilization would need to tap into energy sources unknown to us using strange, or currently unknown, laws of physics.",
-"energy-consumption":"Everything",
-"sci-fi-example":"Olympians? ",
-"image":"olympians.png "},
+    {
+        "type": 4,
+        "summary": "A Type IV civilizations would almost be able to harness the energy content of the entire universe and with that, they could traverse the accelerating expansion of space (furthermore, advance races of these species may live inside supermassive black holes). To previous methods of generating energy, these kinds of feats are considered impossible. A Type IV civilization would need to tap into energy sources unknown to us using strange, or currently unknown, laws of physics.",
+        "energy-consumption": "Everything",
+        "sci-fi-example": "Olympians? ",
+        "image": "olympians.png "
+    },
 
-//Type 5
+    //Type 5
 
-{"Type":5,
-"summary":"A Type V might just be the next possible advancement to such a civilization. Here beings would be like gods, having the knowledge to manipulate the universe as they please. Now, as I said, humans are a very, very long way from ever reaching anything like this. But it’s not to say that it cannot be achieved as long as we take care of Earth and each other. To do so, the first step is to preserve our tiny home, extinguish war, and continue to support scientific advances and discoveries.",
-"energy-consumption":"Who Knows!",
-"sci-fi-example":"Titans?",
-"image":"titans.gif"},
+    {
+        "type": 5,
+        "summary": "A Type V might just be the next possible advancement to such a civilization. Here beings would be like gods, having the knowledge to manipulate the universe as they please. Now, as I said, humans are a very, very long way from ever reaching anything like this. But it’s not to say that it cannot be achieved as long as we take care of Earth and each other. To do so, the first step is to preserve our tiny home, extinguish war, and continue to support scientific advances and discoveries.",
+        "energy-consumption": "Who Knows!",
+        "sci-fi-example": "Titans?",
+        "image": "titans.gif"
+    },
 ];
 
 export default typeArray;
