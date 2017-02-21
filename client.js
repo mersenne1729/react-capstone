aeffcad2928be0adfecf81d4db86c3568059e19b
Thomas <kent.thomas80@gmail.com>
2017-02-21 14:19:41 +0000

    React added

---
+++ client.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var Test = React.createClass({
+  someTest: function () {
+    return {
+    };
+  },
+
+ReactDOM.render(
+<Test/>, 
+  document.getElementById('app')
+);
